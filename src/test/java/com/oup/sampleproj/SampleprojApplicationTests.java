50d65d82f6a6e64a56f05214d1b636fd6bd708a2
harishksinha-oup <harish.sinha@oup.com>
2024-02-20 00:49:39 +0000

    Initial Commit

---
+++ src/test/java/com/oup/sampleproj/SampleprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oup.sampleproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleprojApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
